6a9aaf7009ebdae0746019855b34f3b04cb917c9
Bojan <rodjenlos89@gmail.com>
2021-10-26 17:58:10 +0200

    Category is added

---
+++ CunamiRakite/CunamiRakite.Core/Entities/Category.cs
@@ -0,0 +1,36 @@
+﻿using CSharpFunctionalExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CunamiRakite.Core.Entities
+{
+   public  class Category
+    {
+        public Guid Id { get; }
+        public string Name { get; }
+
+
+        private Category(Guid id, string name) {
+
+            this.Id = id;
+            this.Name = name;
+        }
+
+
+        public static Result<Category> Create(Guid id, string name) {
+
+
+            if (id == Guid.Empty) return Result.Failure<Category>("id cannot be null");
+
+            var catName = CategoryName.Create(name);
+
+            var result = Result.Combine(catName);
+            if (result.IsFailure) return Result.Failure<Category>(result.Error);
+
+            return Result.Success(new Category(id, name));
+        }
+    }
+}
---
+++ CunamiRakite/CunamiRakite.Core/Entities/CategoryName.cs
@@ -0,0 +1,36 @@
+﻿using CSharpFunctionalExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CunamiRakite.Core.Entities
+{
+   public  class CategoryName:ValueObject
+    {
+
+        public string Value { get; }
+
+
+        public  CategoryName(string name) {
+
+            this.Value = name;
+        }
+
+        public static Result<CategoryName> Create(string name) {
+
+
+            if (string.IsNullOrEmpty(name)) return Result.Failure<CategoryName>("cannot be null");
+
+            if (name.Length > 30) return Result.Failure<CategoryName>("cannot be null");
+
+            return Result.Success(new CategoryName(name));
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+    }
+}
